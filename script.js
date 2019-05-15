305ed4138dbd71ead2f92ca6a0b913bac4cca317
supersmitty2018 <mpcschmidtcrans@gmail.com>
2019-05-15 23:25:07 +0200

    Initial commit with first styling and html

---
+++ script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+      $(".topTextDiv").fadeIn(2000);
+      $(".picDiv").fadeIn(3000);
+      $(".firstLineBpt").animate({left: "0.5vw"}, 3000);
+      $(".secondLineBpt").animate({right: "0.5vw"}, 3000);
+      $(".codeAnimate").animate({'fontSize': "50px"}, 3600);
+      $(".codeAnimate").animate({'fontSize': "1em"}, 3750);
+      $(".superContainer").animate({'background': "rgb(255,255,255,0.5"}, 3750);
+    
+  });
