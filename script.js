f62c7e5d45edff15d3a6933d26921444b63202f8
supersmitty2018 <48794241+supersmitty2018@users.noreply.github.com>
2019-05-20 20:33:03 +0200

    Small adjustements to top container

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
       $(".picDiv").fadeIn(3000);
       $(".firstLineBpt").animate({left: "0.5vw"}, 3000);
       $(".secondLineBpt").animate({right: "0.5vw"}, 3000);
-      $(".codeAnimate").animate({'fontSize': "50px"}, 3600);
+      $(".codeAnimate").animate({'fontSize': "0.75em"}, 3600);
       $(".codeAnimate").animate({'fontSize': "1em"}, 3750);
       $(".superContainer").animate({'background': "rgb(255,255,255,0.5"}, 3750);
     
