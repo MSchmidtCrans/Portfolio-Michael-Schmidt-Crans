60796893526cc6adbcdabd47a6e34fe160d193c4
supersmitty2018 <mpcschmidtcrans@gmail.com>
2019-05-20 21:44:14 +0200

    Debugged topnav and jquery animations

--- script.js
+++ script.js
@@ -4,7 +4,5 @@ $(document).ready(function(){
       $(".firstLineBpt").animate({left: "0.5vw"}, 3000);
       $(".secondLineBpt").animate({right: "0.5vw"}, 3000);
       $(".codeAnimate").animate({'fontSize': "0.75em"}, 3600);
-      $(".codeAnimate").animate({'fontSize': "1em"}, 3750);
-      $(".superContainer").animate({'background': "rgb(255,255,255,0.5"}, 3750);
-    
+      $(".codeAnimate").animate({'fontSize': "1em"}, 3750);    
   });
